dfe616479cd6cf6cb4b29294cc207d335a01d005
jwchoi <njaewon@nate.com>
2021-06-26 17:16:12 +0900

    메인화면 TOP 초안

--- src/controls/layout/kokkok.top.conrol.js
+++ src/controls/layout/kokkok.top.conrol.js
@@ -5,11 +5,14 @@ import imgLogo from '../../KokKok_LOGO.png';
 import { 
     Container, CssBaseline, Grid, Typography, Button, TextField, makeStyles, 
     FormControl, FormControlLabel, Checkbox, Link, CircularProgress, Box, AppBar, IconButton, Toolbar } from '@material-ui/core';
-import { green, red } from '@material-ui/core/colors';
+import { green, red, deepOrange } from '@material-ui/core/colors';
+import styles from './kokkok.top.control.css';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        flexGrow: 1,    
+        flexGrow: 1,
+        paddingLeft: 0,
+        paddingRight: 0,
     },
     menuButton: {
         marginRight: theme.spacing(2),
@@ -17,25 +20,28 @@ const useStyles = makeStyles((theme) => ({
     title: {
         flexGrow: 1,
     },
+    appBar: {
+        backgroundColor: deepOrange[500],
+    }
 }));
 
 export default function TopControl() {
     const classes = useStyles();
     
     return (
-        <div className={classes.root}>
-            <CssBaseline />
-            <AppBar position="static">
+        <React.Fragment id="fragment">
+            {/* <CssBaseline /> */}
+            <AppBar id="appbar" position="static" className={classes.appBar}>
                 <Toolbar>
                     <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
                         <MenuIcon />
                     </IconButton>
                     <Typography variant="h6" className={classes.title}>
-                        News
+                        사고싶은거 콕콕
                     </Typography>
                     <Button color="inherit">Login</Button>
                 </Toolbar>
             </AppBar>
-        </div>
+        </React.Fragment>
     );
 }
